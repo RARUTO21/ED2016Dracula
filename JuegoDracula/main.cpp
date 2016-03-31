e06ad32179727dd44419ee1cb9f35d2807037fe2
Alexis <ameyitfabian@gmail.com>
2016-03-30 23:04:40 -0600

    Primer compilada en windows

--- JuegoDracula/main.cpp
+++ JuegoDracula/main.cpp
@@ -1,11 +1,14 @@
 #include "juego.h"
 #include <QApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Juego w;
     w.show();
-
+    cout<<"Hola mundo";
     return a.exec();
 }
---
+++ build-JuegoDracula-Desktop_Qt_5_6_0_MinGW_32bit-Debug/ui_juego.h
@@ -0,0 +1,69 @@
+/********************************************************************************
+** Form generated from reading UI file 'juego.ui'
+**
+** Created by: Qt User Interface Compiler version 5.6.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_JUEGO_H
+#define UI_JUEGO_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Juego
+{
+public:
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QWidget *centralWidget;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *Juego)
+    {
+        if (Juego->objectName().isEmpty())
+            Juego->setObjectName(QStringLiteral("Juego"));
+        Juego->resize(400, 300);
+        menuBar = new QMenuBar(Juego);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        Juego->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(Juego);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        Juego->addToolBar(mainToolBar);
+        centralWidget = new QWidget(Juego);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        Juego->setCentralWidget(centralWidget);
+        statusBar = new QStatusBar(Juego);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        Juego->setStatusBar(statusBar);
+
+        retranslateUi(Juego);
+
+        QMetaObject::connectSlotsByName(Juego);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *Juego)
+    {
+        Juego->setWindowTitle(QApplication::translate("Juego", "Juego", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Juego: public Ui_Juego {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_JUEGO_H
