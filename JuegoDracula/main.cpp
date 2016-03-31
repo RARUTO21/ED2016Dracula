255e1a372d3a028ae2879c4970b509aeeafadd4e
Anthony <Anthonylle@hotmail.com>
2016-03-30 23:10:02 -0700

    Primer compilada en Mac

--- JuegoDracula/main.cpp
+++ JuegoDracula/main.cpp
@@ -1,11 +1,16 @@
 #include "juego.h"
 #include <QApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Juego w;
     w.show();
+
+    cout<<"Hola Mundo!"<<endl;
 
     return a.exec();
 }
