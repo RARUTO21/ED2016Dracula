b6d48b7ae3cb2257c128e894afce34ce7f6a6340
Alexis <ameyitfabian@gmail.com>
2016-03-30 23:19:19 -0600

    Final prueba

--- JuegoDracula/main.cpp
+++ JuegoDracula/main.cpp
@@ -9,12 +9,12 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Juego w;
     w.show();
-<<<<<<< HEAD
+
     cout<<"Hola mundo";
-=======
 
-    cout<<"Hola Mundo!"<<endl;
 
->>>>>>> origin/master
+   // cout<<"Hola Mundo!"<<endl;
+
+
     return a.exec();
 }
