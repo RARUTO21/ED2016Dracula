57625b3d0e3cf224e171fdd4f45e624152e2ac6f
EmmanuelPerezP <emmanuelperezp@gmail.com>
2016-04-17 18:04:37 -0600

    arreglar las puntuaciones v2

--- JuegoDracula/juego.h
+++ JuegoDracula/juego.h
@@ -74,7 +74,6 @@ private slots:
 
     void on_btnEvalua_clicked();
 
-    void on_pushButton_2_clicked();
 
     void on_ImprimeConsola_clicked();
 
--- build-JuegoDracula-Desktop-Debug/moc_juego.cpp
+++ build-JuegoDracula-Desktop-Debug/moc_juego.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_Juego_t {
-    QByteArrayData data[23];
-    char stringdata0[517];
+    QByteArrayData data[22];
+    char stringdata0[493];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -50,8 +50,7 @@ QT_MOC_LITERAL(17, 380, 21), // "on_btnMatriz7_clicked"
 QT_MOC_LITERAL(18, 402, 21), // "on_btnMatriz8_clicked"
 QT_MOC_LITERAL(19, 424, 21), // "on_btnMatriz9_clicked"
 QT_MOC_LITERAL(20, 446, 20), // "on_btnEvalua_clicked"
-QT_MOC_LITERAL(21, 467, 23), // "on_pushButton_2_clicked"
-QT_MOC_LITERAL(22, 491, 25) // "on_ImprimeConsola_clicked"
+QT_MOC_LITERAL(21, 467, 25) // "on_ImprimeConsola_clicked"
 
     },
     "Juego\0correrReloj\0\0on_btnMazo_clicked\0"
@@ -67,8 +66,7 @@ QT_MOC_LITERAL(22, 491, 25) // "on_ImprimeConsola_clicked"
     "on_btnMatriz3_clicked\0on_btnMatriz4_clicked\0"
     "on_btnMatriz6_clicked\0on_btnMatriz7_clicked\0"
     "on_btnMatriz8_clicked\0on_btnMatriz9_clicked\0"
-    "on_btnEvalua_clicked\0on_pushButton_2_clicked\0"
-    "on_ImprimeConsola_clicked"
+    "on_btnEvalua_clicked\0on_ImprimeConsola_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -78,7 +76,7 @@ static const uint qt_meta_data_Juego[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      21,   14, // methods
+      20,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -86,30 +84,28 @@ static const uint qt_meta_data_Juego[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  119,    2, 0x0a /* Public */,
-       3,    0,  120,    2, 0x08 /* Private */,
-       4,    0,  121,    2, 0x08 /* Private */,
-       5,    0,  122,    2, 0x08 /* Private */,
-       6,    0,  123,    2, 0x08 /* Private */,
-       7,    0,  124,    2, 0x08 /* Private */,
-       8,    0,  125,    2, 0x08 /* Private */,
-       9,    0,  126,    2, 0x08 /* Private */,
-      10,    0,  127,    2, 0x08 /* Private */,
-      11,    0,  128,    2, 0x08 /* Private */,
-      12,    0,  129,    2, 0x08 /* Private */,
-      13,    0,  130,    2, 0x08 /* Private */,
-      14,    0,  131,    2, 0x08 /* Private */,
-      15,    0,  132,    2, 0x08 /* Private */,
-      16,    0,  133,    2, 0x08 /* Private */,
-      17,    0,  134,    2, 0x08 /* Private */,
-      18,    0,  135,    2, 0x08 /* Private */,
-      19,    0,  136,    2, 0x08 /* Private */,
-      20,    0,  137,    2, 0x08 /* Private */,
-      21,    0,  138,    2, 0x08 /* Private */,
-      22,    0,  139,    2, 0x08 /* Private */,
+       1,    0,  114,    2, 0x0a /* Public */,
+       3,    0,  115,    2, 0x08 /* Private */,
+       4,    0,  116,    2, 0x08 /* Private */,
+       5,    0,  117,    2, 0x08 /* Private */,
+       6,    0,  118,    2, 0x08 /* Private */,
+       7,    0,  119,    2, 0x08 /* Private */,
+       8,    0,  120,    2, 0x08 /* Private */,
+       9,    0,  121,    2, 0x08 /* Private */,
+      10,    0,  122,    2, 0x08 /* Private */,
+      11,    0,  123,    2, 0x08 /* Private */,
+      12,    0,  124,    2, 0x08 /* Private */,
+      13,    0,  125,    2, 0x08 /* Private */,
+      14,    0,  126,    2, 0x08 /* Private */,
+      15,    0,  127,    2, 0x08 /* Private */,
+      16,    0,  128,    2, 0x08 /* Private */,
+      17,    0,  129,    2, 0x08 /* Private */,
+      18,    0,  130,    2, 0x08 /* Private */,
+      19,    0,  131,    2, 0x08 /* Private */,
+      20,    0,  132,    2, 0x08 /* Private */,
+      21,    0,  133,    2, 0x08 /* Private */,
 
  // slots: parameters
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -159,8 +155,7 @@ void Juego::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void 
         case 16: _t->on_btnMatriz8_clicked(); break;
         case 17: _t->on_btnMatriz9_clicked(); break;
         case 18: _t->on_btnEvalua_clicked(); break;
-        //case 19: _t->on_pushButton_2_clicked(); break;
-        case 20: _t->on_ImprimeConsola_clicked(); break;
+        case 19: _t->on_ImprimeConsola_clicked(); break;
         default: ;
         }
     }
@@ -192,13 +187,13 @@ int Juego::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 21)
+        if (_id < 20)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 21;
+        _id -= 20;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 21)
+        if (_id < 20)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 21;
+        _id -= 20;
     }
     return _id;
 }
